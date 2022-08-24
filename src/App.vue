38d89a4afbdad1fa3986a318ceeb14b0a17bd499
IlyaChichkov <ilya555chich@yandex.ru>
2022-08-24 13:19:38 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <QRCodeCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './App.css'
+import QRCodeCard from "@/components/QRCodeCard";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QRCodeCard
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/QRCodeCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <img class="" src="../assets/image-qr-code.png">
+    <div class="px-3">
+      <h3 class="main-txt">Improve your front-end skills by building projects</h3>
+      <p class="sec-txt">Scan th QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QRCodeCard"
+}
+</script>
+
+<style scoped>
+.card img{
+  @apply rounded-2xl;
+}
+.card{
+  @apply flex flex-col items-center;
+  @apply p-4 w-[86%] sm:w-[340px] xl:w-[340px] bg-white rounded-3xl mx-6;
+  @apply text-center;
+}
+.main-txt{
+  @apply mt-6 text-dark-blue text-2xl font-bold;
+}
+.sec-txt{
+  @apply mt-4 mb-6 text-[15px] text-blue font-normal;
+}
+</style>
